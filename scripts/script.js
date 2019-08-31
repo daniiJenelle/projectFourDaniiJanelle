48ebb99b35430b51e66ee8386843c262ad31671b
Jenelle <lawjenelle@gmail.com>
2019-08-31 09:13:53 -0400

    switched out both city APIs for more functional ones which include more sanitized data. still needs some error handling for countries like Hong Kong

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,41 @@
-const app = {};
-app.chosenCity = [];
+app = {};
+app.chosenCityName = '';
+// app.chosenCityInfo = [];
 
-// function to render list of possible city name matches based on user input defined here
+// AJAX METHODS START
+
+// ajax call to get possible city names
+app.getCityNames = function (cityName) {
+  return $.ajax({
+    url: `http://gd.geobytes.com/AutoCompleteCity`,
+    method: 'GET',
+    dataType: 'jsonp',
+    headers: {
+      "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
+      "x-rapidapi-key": "9e16543852msh061f067cf4ff492p109400jsn392cd80914f3"
+    },
+    data: {
+      q: cityName
+    }
+  })
+};
+
+// ajax call to get information about city based on chosen city name
+app.getCityInfo = function (chosenCity) {
+  return $.ajax({
+    url: `http://dataservice.accuweather.com/locations/v1/cities/search`,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      apikey: 'T18lCvOwKFBANPDKG6JET6LQJ6TrHwrO',
+      q: chosenCity
+    }
+  });
+}; // AJAX METHODS END
+
+// METHODS START
+
+// listen for form submit when the user searches for a city name
 app.formSubmit = function () {
   $(`form`).on(`submit`, function (e) {
     e.preventDefault();
@@ -9,139 +43,71 @@ app.formSubmit = function () {
     app.chosenCity = [];
 
     let cityName = ($(`:text`).val());
-    app.findCityAPICall(cityName);
-
+    app.searchCityAutocomplete(cityName)
   });
 };
 
-// function to determine the correct object that holds the city information which matches user search
-app.findCityAPICall = async function (cityName) {
-  // this will run ajax call and return an array of cities which matches the user input
-  let cityInfo = await app.getCityInfo(cityName);
-  console.log(`Freshly retrieved data`, cityInfo);
-  app.handleCityInfo(cityInfo);
+// invokes search to returned city names which match search query
+app.searchCityAutocomplete = async function (cityName) {
+  let matchedCities = await app.getCityNames(cityName);
+  console.log('FRESHLY RETRIEVED DATA', matchedCities);
+  app.handleMatchedCities(matchedCities);
 };
 
-app.handleCityInfo = function (cityInfo) {
-  cityInfo.Results.forEach((city) => {
-    if (city.name.includes(`null`)) {
-      city.name = city.name.replace(`(null)`, city.c)
-    }
-  });
-  if (cityInfo.Results.length > 1) {
-    // renders all cities that matches user input to the DOM
-    app.renderCityList(cityInfo);
-    app.chooseCityFromList(cityInfo);
+// checks data from returned city names
+app.handleMatchedCities = function (matchedCities) {
+  if (matchedCities.length > 1) {
+    // render all cities that matches user input to the DOM
+    app.renderMatchedCitiesList(matchedCities);
+    app.chooseCityFromList(matchedCities);
   } else {
-    app.chosenCity = cityInfo.Results
-    assignmentFunction(app.chosenCity);
+    app.chosenCityName = matchedCities;
+    app.searchHandleCityInfo(app.chosenCityName);
   }
 }
 
-// function to print list of cities on page that match user input
-app.renderCityList = function (cityInfo) {
-  cityInfo.Results.forEach((city) => {
-    const liHTML = `<li><a>${city.name}</a></li>`
+// if more than 1 matched city, print list of cities on page that match user input
+app.renderMatchedCitiesList = function (matchedCities) {
+  matchedCities.forEach((city) => {
+    const liHTML = `<li><a>${city}</a></li>`
     // prints each city as a list item on page
     $('.cityOptions').append(liHTML);
   })
 }
 
-// function to listen for which matched city the user clicks on
-app.chooseCityFromList = function (cityInfo) {
+// listen for which matched city the user clicks on
+app.chooseCityFromList = function (matchedCities) {
   $('.cityOptions').on('click', 'li', function () {
-    app.chosenCity = cityInfo.Results.filter((city) => {
-      return city.name === $(this).text();
+    app.chosenCityName = matchedCities.filter((city) => {
+      return city === $(this).text();
     });
-    console.log(app.chosenCity);
-    assignmentFunction(app.chosenCity);
+    console.log('User has selected this city', app.chosenCityName)
+    app.searchHandleCityInfo(app.chosenCityName);
     $('.cityOptions').off();
   });
 }
 
-// assignment function to grab the needed properties from our chosen city object
-const assignmentFunction = (chosenCity) => {
-  app.placeName = chosenCity[0].name;
-  app.countryCode = chosenCity[0].c;
-  app.latitude = chosenCity[0].lat;
-  app.longitude = chosenCity[0].lon;
+// use chosen city to invoke search to retrieve city information
+app.searchHandleCityInfo = async function (chosenCity) {
+  let chosenCityInfo = await app.getCityInfo(chosenCity);
+  console.log('Info of chosen city has been stored', chosenCityInfo);
+  app.officialCityName = chosenCityInfo[0].EnglishName;
+  app.countryName = chosenCityInfo[0].Country.EnglishName;
+  app.countryCode = chosenCityInfo[0].Country.ID;
+  app.latitude = chosenCityInfo[0].GeoPosition.Latitude;
+  app.longitude = chosenCityInfo[0].GeoPosition.Longitude;
 
-  // app.dashboardAPICalls(app.countryCode, app.latitude, app.longitude);
+  console.log(app.officialCityName, app.countryName, app.countryCode, app.latitude, app.longitude);
 }
 
-app.dashboardAPICalls = async function (cityName, latitude, longitude) {
-  const news = await app.getNews(cityName);
-  const time = await app.getTimezone(latitude, longitude);
-  const weather = await app.getWeather(latitude, longitude);
-  // console.log(news, time, weather)
-}
+// app.dashboardAPICalls(app.countryCode, app.latitude, app.longitude);
 
-// function for ajax call to get information about city based on city name
-app.getCityInfo = function (cityName) {
-  return $.ajax({
-    url: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php`,
-    method: 'GET',
-    dataType: 'json',
-    headers: {
-      "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-      "x-rapidapi-key": "9e16543852msh061f067cf4ff492p109400jsn392cd80914f3"
-    },
-    data: {
-      location: cityName
-    }
-  });
-};
-
-// funtion for ajax call to get news based on country code
-
-app.getNews = function (cityName) {
-  return $.ajax({
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json`,
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      'api-key': '25f7AaRW3nMz4B6VGWlyf69GrcGkw2Ee',
-      q: cityName
-    }
-  });
-};
-
-// function for ajax call to get timezone information based on latitude and longitutde
-app.getTimezone = function (latitude, longitude) {
-  return $.ajax({
-    url: `http://api.timezonedb.com/v2.1/get-time-zone`,
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      key: '8EZVCD1H71OJ',
-      format: 'json',
-      by: 'position',
-      lat: latitude,
-      lng: longitude
-    }
-  });
-};
-
-// function for ajax call to get weather based on latitude and longitude
-app.getWeather = function (latitude, longitude) {
-  return $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather`,
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      APPID: '1a8d4351dec8a304fba8b7b27b31933e',
-      lat: latitude,
-      lon: longitude,
-      format: 'json'
-    }
-  });
-};
-
-
+// INIT FUNCTION
 app.init = function () {
   app.formSubmit();
 };
 
+// DOCUMENT READY
 $(function () {
   app.init()
 
